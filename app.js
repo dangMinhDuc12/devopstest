6011720f95995e138ce49b8225e337f26c0d3817
Duc Dang <dangminhduca3@gmail.com>
2024-02-21 09:19:13 +0700

    test change code

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ const express = require('express');
 const app = express();
 const { Pool } = require('pg');
 
-// const pool = new Pool({
-//   user: 'postgres',
-//   host: 'postgresdb',
-//   database: 'postgres',
-//   password: 'postgres',
-//   port: 5432,
-// });
+const pool = new Pool({
+  user: 'your_username',
+  host: 'postgres',
+  database: 'your_database_name',
+  password: 'ZnJQQjM3MmRDZ0RSY1RyaQ==',
+  port: 5432
+});
 
 app.get('/heartbeat', async (req, res, next) => {
-  // const test = await pool.query('SELECT NOW()');
-  return res.status(200).send('Heartbeat routeee');
+  const test = await pool.query('SELECT NOW()');
+  return res.status(200).send(test);
 });
 
 app.listen(3000, () => {
