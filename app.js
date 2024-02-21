5bbe351b0b4a4d96b352f8f08c561edb43b59cf4
Duc Dang <dangminhduca3@gmail.com>
2024-02-21 09:37:30 +0700

    Add swagger

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const app = express();
 const { Pool } = require('pg');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+const swaggerUi = require('swagger-ui-express');
 
-const pool = new Pool({
-  user: 'your_username',
-  host: 'postgres',
-  database: 'your_database_name',
-  password: 'ZnJQQjM3MmRDZ0RSY1RyaQ==',
-  port: 5432
-});
+// const pool = new Pool({
+//   user: 'your_username',
+//   host: 'postgres',
+//   database: 'your_database_name',
+//   password: 'ZnJQQjM3MmRDZ0RSY1RyaQ==',
+//   port: 5432
+// });
 
+
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.get('/heartbeat', async (req, res, next) => {
-  const test = await pool.query('SELECT NOW()');
-  return res.status(200).send(test);
+  // const test = await pool.query('SELECT NOW()');
+  return res.status(200).send('oke');
 });
 
 app.listen(3000, () => {
