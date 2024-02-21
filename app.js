3d871c438ed1b88a43bfe7d2993d1457976c8284
Duc Dang <dangminhduca3@gmail.com>
2024-02-21 13:20:44 +0700

    Add env to jenkinsfile

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const { Pool } = require('pg');
@@ -5,19 +6,19 @@ const YAML = require('yamljs');
 const swaggerDocument = YAML.load('./swagger.yaml');
 const swaggerUi = require('swagger-ui-express');
 
-// const pool = new Pool({
-//   user: 'your_username',
-//   host: 'postgres',
-//   database: 'your_database_name',
-//   password: 'ZnJQQjM3MmRDZ0RSY1RyaQ==',
-//   port: 5432
-// });
+const pool = new Pool({
+  user: process.env.DB_USERNAME,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: 5432
+});
 
 
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.get('/heartbeat', async (req, res, next) => {
-  // const test = await pool.query('SELECT NOW()');
-  return res.status(200).send('oke');
+  const test = await pool.query('SELECT NOW()');
+  return res.status(200).send(test);
 });
 
 app.listen(3000, () => {
